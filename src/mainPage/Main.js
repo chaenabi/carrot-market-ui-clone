e3b2dbf298abf37d332697ed935998be1bbbfbaa
chaenabi <find1086@gmail.com>
2020-06-21 20:23:14 +0900

    semi-design main-hot-article css

--- src/mainPage/Main.js
+++ src/mainPage/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MainHeader from "./MainHeader";
 import MainTitle from "./MainTitle";
+import MainHotArticle from "./MainHotArticle";
 import './reset.css';
 import './main.css';
 
@@ -10,6 +11,7 @@ function Main () {
         <>
          <MainHeader/>
          <MainTitle/>
+         <MainHotArticle/>
         </>
     );
 };
---
+++ src/mainPage/MainHotArticle.jsx
@@ -0,0 +1,211 @@
+import React from 'react';
+
+function MainHotArticle() {
+
+    return (
+        <>
+            <section id="hot-articles">
+                <h3>당근마켓 인기 매물</h3>
+                <div className="title-line-divider"></div>
+                <section className="cards-wrap">
+                    <article className="card-top ">
+                        <a className="card-link " href="#">
+                            <div className="card-photo">
+                                <img alt="티키타카 자전거 판매합니다" src="https://dnvefa72aowie.cloudfront.net/origin/article/202006/927bc42c8bdd5f09d5920b6191e974cdf511582e4088dc5a1725fd21099ecbf0.webp?q=82&amp;s=300x300&amp;t=crop"/>
+                            </div>
+                            <div className="card-desc">
+                                <h2 className="card-title">티키타카 자전거 판매합니다</h2>
+                                <div className="card-region-name">
+                                    서울 송파구 문정동
+                                </div>
+                                <div className="card-price ">
+                                    53,000원
+                                </div>
+                                <div className="card-counts">
+                  <span>
+                    관심 19
+                  </span>
+                                    ∙
+                                    <span>
+                    채팅 51
+                  </span>
+                                </div>
+                            </div>
+                        </a></article>
+                    <article className="card-top ">
+                    <a className="card-link " href="#">
+                        <div className="card-photo">
+                            <img alt="LG김치냉장고" src="https://dnvefa72aowie.cloudfront.net/origin/article/202006/d13ab185de334006f9add682338ee6a88ffa2e73dc508369f3a621a8f821b254.webp?q=82&amp;s=300x300&amp;t=crop"/>
+                        </div>
+                        <div className="card-desc">
+                            <h2 className="card-title">LG김치냉장고</h2>
+                            <div className="card-region-name">
+                                경기도 고양시 일산서구 대화동
+                            </div>
+                            <div className="card-price ">
+                                20,000원
+                            </div>
+                            <div className="card-counts">
+                  <span>
+                    관심 41
+                  </span>
+                                ∙
+                                <span>
+                    채팅 61
+                  </span>
+                            </div>
+                        </div>
+                    </a></article><article className="card-top ">
+                    <a className="card-link " href="#">
+                        <div className="card-photo">
+                            <img alt="고급캣타워" src="https://dnvefa72aowie.cloudfront.net/origin/article/202006/a13bbd9424f32fb8bd74bf5568e7d8af6e6a0f536f3423a566061999a8a945cc.webp?q=82&amp;s=300x300&amp;t=crop"/>
+                        </div>
+                        <div className="card-desc">
+                            <h2 className="card-title">고급캣타워</h2>
+                            <div className="card-region-name">
+                                서울 송파구 가락1동
+                            </div>
+                            <div className="card-price ">
+                                50,000원
+                            </div>
+                            <div className="card-counts">
+                  <span>
+                    관심 15
+                  </span>
+                                ∙
+                                <span>
+                    채팅 40
+                  </span>
+                            </div>
+                        </div>
+                    </a></article><article className="card-top ">
+                    <a className="card-link " href="#">
+                        <div className="card-photo">
+                            <img alt="선반" src="https://dnvefa72aowie.cloudfront.net/origin/article/202006/7c3dad604d7772180f706339867e102105a2881cd894233626575ee91f88e5dc.webp?q=82&amp;s=300x300&amp;t=crop"/>
+                        </div>
+                        <div className="card-desc">
+                            <h2 className="card-title">선반</h2>
+                            <div className="card-region-name">
+                                경기도 화성시 석우동
+                            </div>
+                            <div className="card-price ">
+                                10,000원
+                            </div>
+                            <div className="card-counts">
+                  <span>
+                    관심 31
+                  </span>
+                                ∙
+                                <span>
+                    채팅 30
+                  </span>
+                            </div>
+                        </div>
+                    </a></article><article className="card-top ">
+                    <a className="card-link " href="#">
+                        <div className="card-photo">
+                            <img alt="포트메리온" src="https://dnvefa72aowie.cloudfront.net/origin/article/202006/20ce9db434a2d0d82a2d0395227d889bd08be2047544f4fa8eed5801326161f6.webp?q=82&amp;s=300x300&amp;t=crop"/>
+                        </div>
+                        <div className="card-desc">
+                            <h2 className="card-title">포트메리온</h2>
+                            <div className="card-region-name">
+                                서울 영등포구 여의동
+                            </div>
+                            <div className="card-price ">
+                                10,000원
+                            </div>
+                            <div className="card-counts">
+                  <span>
+                    관심 9
+                  </span>
+                                ∙
+                                <span>
+                    채팅 29
+                  </span>
+                            </div>
+                        </div>
+                    </a></article><article className="card-top ">
+                    <a className="card-link " href="#">
+                        <div className="card-photo">
+                            <img alt="캠핑테이블 간이의자" src="https://dnvefa72aowie.cloudfront.net/origin/article/202006/7b8d1b622eebc789757a5656da5e91efe8e395bcc691567cf6695a17bd009698.webp?q=82&amp;s=300x300&amp;t=crop"/>
+                        </div>
+                        <div className="card-desc">
+                            <h2 className="card-title">캠핑테이블 간이의자</h2>
+                            <div className="card-region-name">
+                                인천 연수구 연수동
+                            </div>
+                            <div className="card-price ">
+                                8,000원
+                            </div>
+                            <div className="card-counts">
+                  <span>
+                    관심 2
+                  </span>
+                                ∙
+                                <span>
+                    채팅 24
+                  </span>
+                            </div>
+                        </div>
+                    </a></article><article className="card-top ">
+                    <a className="card-link" href="#" >
+                        <div className="card-photo">
+                            <img alt="캠핑 테이블 " src="https://dnvefa72aowie.cloudfront.net/origin/article/202006/010485A89F21092548D5E57A4D12B14430F51E14A96D9E4F818F2DA454BA6580.jpg?q=82&amp;s=300x300&amp;t=crop"/>
+                        </div>
+                        <div className="card-desc">
+                            <h2 className="card-title">캠핑 테이블 </h2>
+                            <div className="card-region-name">
+                                서울 송파구 잠실4동
+                            </div>
+                            <div className="card-price ">
+                                10,000원
+                            </div>
+                            <div className="card-counts">
+                  <span>
+                    관심 6
+                  </span>
+                                ∙
+                                <span>
+                    채팅 22
+                  </span>
+                            </div>
+                        </div>
+                    </a></article><article className="card-top">
+                    <a className="card-link" href="#">
+                        <div className="card-photo">
+                            <img alt="전동드릴 새제품" src="https://dnvefa72aowie.cloudfront.net/origin/article/202006/6dd9d4729a4940b8b6addf6a6ec0b28d2191c90fc62adb87c89834854fbac697.webp?q=82&amp;s=300x300&amp;t=crop"/>
+                        </div>
+                        <div className="card-desc">
+                            <h2 className="card-title">전동드릴 새제품</h2>
+                            <div className="card-region-name">
+                                경기도 하남시 미사2동
+                            </div>
+                            <div className="card-price ">
+                                30,000원
+                            </div>
+                            <div className="card-counts">
+                  <span>
+                    관심 3
+                  </span>
+                                ∙
+                                <span>
+                    채팅 23
+                  </span>
+                            </div>
+                        </div>
+                    </a></article>
+                    <div className="card-top " id="cards-more">
+                        <a className="card-link" href="#">
+                            <div id="cards-more-text">
+                                <img alt="+" src="https://d1unjqcospf8gs.cloudfront.net/assets/home/base/mobile/icon-add-dcfb7aac98f18d19ca4c27e9dc29c63cf73861f3b10e0a42c06163d2658d0691.svg"/>
+                                <div id="more-text">당근마켓<br/>인기 매물<br/>더 보기</div>
+                            </div>
+                        </a></div>
+
+                </section>
+            </section>
+        </>
+);
+}
+
+export default MainHotArticle;
